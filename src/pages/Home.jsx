a14fdb8858ed1e12fdd01c9ebe2953c9acdf0e25
lilJ <julia.s@live.nl>
2025-07-27 16:30:13 +0200

    organize layout home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,9 @@
 const Home = () => {
   return (
     <>
-      <div className="bg-banana text-center">
-        <h1 className="pt-10 font-pangaia tracking-tight text-balance text-peach sm:text-5xl ">
+      <div className="bg-banana text-left">
+        <div className="max-w-3xl mx-auto px-4 pt-6">
+          <h1 className="pt-10 font-pangaia tracking-tight text-balance text-peach sm:text-5xl ">
           Schoonheid is overal
         </h1>
         <p className="mt-8 font-nunito text-gray-500 sm:text-xl/8">
@@ -15,14 +16,6 @@ const Home = () => {
         <br />
         Als je het wilt en kunt zien, is er schoonheid in vrijwel alle wezens en dingen.
         </p>
-
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            Get started
-          </a>
-          <a href="#" className="text-sm/6 font-semibold text-gray-900">
-            Learn more <span aria-hidden="true">→</span>
-          </a>
         </div>
       </div>
     </>

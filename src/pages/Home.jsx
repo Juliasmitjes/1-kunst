9ff2ce664b231afc98cffc9cb56ae426a754d364
lilJ <julia.s@live.nl>
2025-07-27 20:43:32 +0200

    changes to stylesheet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@
 import { useOutletContext } from 'react-router-dom'
 import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom'
+import styles from '../styles/home.module.css'
 
 
 const Home = () => {
@@ -15,24 +16,30 @@ const Home = () => {
         <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 px-4 pt-6 items-center">      
           <div>
 
-            <h1 className="pt-24 font-pangaia tracking-tight text-balance text-peach sm:text-6xl">
+            <div className={styles.header}>
+            <h1 className="pt-24 tracking-tight text-balance text-6xl">
               Schoonheid is overal
-            </h1>
-            <p className="mt-8 font-nunito text-gray-500 sm:text-xl/8 pb-10 pr-6">
-              Als dromerig meisje had ik oog voor de schoonheid van het alledaagse: de schittering van kiezels, de ruwe bast van een oude boom vol knoesten, het gracieuze van een naaktslak… In een tuincentrum koop ik vaak een lelijk plantje. ‘Kom maar kleintje, ik zoek een mooie plek voor je en zal je laten stralen.’
-              <br /><br />
-              Als je het wilt en kunt zien, is er schoonheid in vrijwel alle wezens en dingen.
-            </p>
+            </h1> 
+            </div>
+
+            <div className={styles.text}>
+            <p className="mt-8 pb-10 pr-6">
+            Als dromerig meisje had ik oog voor de schoonheid van het alledaagse: de schittering van kiezels, de ruwe bast van een oude boom vol knoesten, het gracieuze van een naaktslak… In een tuincentrum koop ik vaak een lelijk plantje. ‘Kom maar kleintje, ik zoek een mooie plek voor je en zal je laten stralen.’
+            <br /><br />
+            Als je het wilt en kunt zien, is er schoonheid in vrijwel alle wezens en dingen.
+            </p>              
+            </div>   
+          
 
             <div className="flex space-x-4">
 
-               <button
-                  type="button"
-                  onClick={() => setShowContactForm(true)}
-                  className="rounded-md bg-peach px-4 py-2 text-xl font-nunito text-white hover:bg-sky transition"
-                >
-                  Meer informatie
-                </button>
+            <button
+              type="button"
+              onClick={() => setShowContactForm(true)}
+              className="rounded-md bg-peach px-4 py-2 text-xl font-nunito text-white hover:bg-sky transition"
+            >
+              Meer informatie
+            </button>
 
 
            <Link
@@ -51,10 +58,25 @@ const Home = () => {
               alt="Vrouw schildert kunstwerk"
             />
           </div>
-      </div>
-      <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 px-4 pt-6 items-center">  
+          </div>
 
-      </div>
+          <div className={styles.header}>
+          <h1 className="mx-auto pt-24 tracking-tight text-balance">
+          Van groene joekel tot verdord skelet 
+          </h1>
+          </div>
+          
+          <div className={styles.text}>
+          <p className="max-w-4xl mx-auto mt-6 text-align-left">  
+          Kerst het feest van licht, goud en glitters én van kerstbomen. Bij ons thuis altijd twee stuks, Nordmannen, een kleintje in de voorkamer en een grote joekel in de achterkamer. Bos in huis, dennengeur, het groen,  zo fijn, maar ook het trieste vooruitzicht: over een paar weken de bomen afgedankt bij het vuil als verdorde skeletten. 
+          </p>
+          </div>
+      
+
+
+
+ 
+     
 
     </div>
 

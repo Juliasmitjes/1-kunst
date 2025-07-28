62b366d104fa2a96a4f5fe5045304f09be8a4ae3
lilJ <julia.s@live.nl>
2025-07-28 08:23:39 +0200

    change image homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,15 +17,15 @@ const Home = () => {
 
             <div className={styles.header}>
             <h1 className="pt-24 tracking-tight text-balance text-6xl text-peach">
-              Schoonheid is overal
+              Moeder Accountant Kunstenaar 
             </h1> 
             </div>
 
             <div className={styles.text}>
             <p className="mt-8 pb-10 pr-6">
-            Als dromerig meisje had ik oog voor de schoonheid van het alledaagse: de schittering van kiezels, de ruwe bast van een oude boom vol knoesten, het gracieuze van een naaktslak… In een tuincentrum koop ik vaak een lelijk plantje. ‘Kom maar kleintje, ik zoek een mooie plek voor je en zal je laten stralen.’
+            Ik ben gek op het leven en vind ook veel leuk: getallen, taal, kinderen, kunst, … Soms is het lastig kiezen. Gelukkig hoeft ook niet alles tegelijk. Mijn leven is een avontuurlijke reis van accountant/controller, moeder en, nu de kinderen zijn uitgevlogen, als kunstenaar. Hoe heerlijk! Vorm kleur en creativiteit. Met hart en handen. 
             <br /><br />
-            Als je het wilt en kunt zien, is er schoonheid in vrijwel alle wezens en dingen.
+            Stiekem heb ik altijd een ambacht willen uitoefenen. 
             </p>              
             </div>   
           
@@ -51,7 +51,6 @@ const Home = () => {
             </div>            
           </div>
 
-
           <div className="max-h-[630px] overflow-hidden rounded-md">
             <img
               className="w-full h-full object-cover"
@@ -61,62 +60,25 @@ const Home = () => {
           </div>
         </div>
 
-      <div className="max-w-7xl mx-auto mt-3 bg-blue text-left p-4 pl-10 rounded-lg">
+        <div className="max-w-7xl mx-auto mt-3">
+          <img
+            className="w-full h-full object-cover max-h-[330px] rounded-lg brightness-110 transition"
+            src="/images/painting.jpg" 
+            alt="Schilderij"
+          />            
+        </div>
+
+      <div className="max-w-7xl mx-auto mt-3 bg-peach text-left p-4 pl-10 rounded-lg">
         <div className={styles.header}>
-          <h1 className="pt-10 tracking-tight text-balance text-6xl text-sky">
-          Van groene joekel tot verdord skelet
+          <h1 className="pt-10 tracking-tight text-balance text-6xl text-white">
+          Ontdek Onze Bomen
           </h1> 
         </div>
-        <div className={styles.text}>
-          <p className="mt-8 pb-10 pr-6 ">
-          Kerst het feest van licht, goud en glitters én van kerstbomen. Bij ons thuis altijd twee stuks, Nordmannen, een kleintje in de voorkamer en een grote joekel in de achterkamer. Bos in huis, dennengeur, het groen,  zo fijn, maar ook het trieste vooruitzicht: over een paar weken de bomen afgedankt bij het vuil als verdorde skeletten. 
-          </p>
-        </div>
       </div>
-      
-      <div className="grid grid-cols-1 lg:grid-cols-5 max-w-7xl mx-auto mt-3">
-          <img
-            className="w-full h-full object-cover rounded-none rounded-tl-xl rounded-bl-xl hover:brightness-75 transition"
-            src="/images/natuurlijk1.jpg" 
-            alt="Stof"
-          />
-            <img
-            className="w-full h-full object-cover rounded-none hover:brightness-75 transition"
-            src="/images/natuurlijk2.jpg" 
-            alt="Boom"
-          />
-            <img
-            className="w-full h-full object-cover rounded-none hover:brightness-75 transition" 
-            src="/images/natuurlijk3.jpg" 
-            alt="Kaars"
-          />
-          <img
-            className="w-full h-full object-cover rounded-none hover:brightness-75 transition"
-            src="/images/natuurlijk4.jpg" 
-            alt="Kaars"
-          />
-          <img
-            className="w-full h-full object-cover rounded-none rounded-tr-xl rounded-br-xl hover:brightness-75 transition"
-            src="/images/natuurlijk5.jpg" 
-            alt="Kaars"
-          />
-      </div>
+  
 
 
-      <div className= "bg-banana mx-auto max-w-7xl items-center justify-between p-6 mt-3 mb-3 lg:px-8 rounded-lg">
-        <div className={styles.header}>
-          <h1 className="pt-10 tracking-tight text-balance text-6xl">
-          Van groene joekel tot verdord skelet
-          </h1> 
-        </div>
-        <div className={styles.text}>
-          <p className="mt-8 pb-10 pr-6 ">
-          Kerst het feest van licht, goud en glitters én van kerstbomen. Bij ons thuis altijd twee stuks, Nordmannen, een kleintje in de voorkamer en een grote joekel in de achterkamer. Bos in huis, dennengeur, het groen,  zo fijn, maar ook het trieste vooruitzicht: over een paar weken de bomen afgedankt bij het vuil als verdorde skeletten. 
-          </p>
-        </div>
 
-
-      </div>
 
     </>
   );

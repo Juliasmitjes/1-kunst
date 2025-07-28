ea8285dd0ffa1048b2be2ec250f3c509959cbd68
lilJ <julia.s@live.nl>
2025-07-28 08:42:10 +0200

    add js effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,31 @@ import { useOutletContext } from 'react-router-dom'
 import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom'
 import styles from '../styles/home.module.css'
+import { useEffect, useRef } from "react";
 
 
 const Home = () => {
 
-  const { setShowContactForm } = useOutletContext()
+  const { setShowContactForm } = useOutletContext();
+
+  useEffect(() => {
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("translate-y-20");
+        entry.target.classList.add("-1translate-y-6");
+        observer.unobserve(entry.target); // Eenmalige actie
+      }
+    });
+  });
+
+  const target = document.getElementById("textDiscover");
+  if (target) {
+    observer.observe(target);
+  }
+
+  return () => observer.disconnect();
+  }, []); 
 
   return (
     <>
@@ -68,18 +88,13 @@ const Home = () => {
           />            
         </div>
 
-      <div className="max-w-7xl mx-auto mt-3 bg-peach text-left p-4 pl-10 rounded-lg">
+      <div  className="max-w-7xl mx-auto mt-3 bg-peach text-left p-4 pl-10 rounded-lg">
         <div className={styles.header}>
-          <h1 className="pt-10 tracking-tight text-balance text-6xl text-white">
+          <h1 id="textDiscover" className="pt-10 tracking-tight text-balance text-6xl text-white transition transform translate-y-20 duration-500">
           Ontdek Onze Bomen
           </h1> 
         </div>
       </div>
-  
-
-
-
-
     </>
   );
 };

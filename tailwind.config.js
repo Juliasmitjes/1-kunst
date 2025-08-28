66fb627822dd63ff808a5160995370436ac8e193
lilJ <julia.s@live.nl>
2025-08-28 19:25:31 +0200

    color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ export default {
     extend: {
       colors: {
         banana: '#FAF6E9',
-        peach: '#A0C878',
+        sky: '#A0C878',
         blue: '#DDEB9D',
-        sky: '#5A8F4B',
+        peach: '#5A8F4B',
         white: '#FFFDF6',
       },
       fontFamily: {

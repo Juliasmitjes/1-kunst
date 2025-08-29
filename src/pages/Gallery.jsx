a340cf9cd45223c96e9920fabd05267b872880b9
lilJ <julia.s@live.nl>
2025-08-29 11:30:12 +0200

    finalize about me page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -140,16 +140,46 @@ const Gallery = () => {
 
             <Link
               to="/bomen"
-              className="max-w-xl inline-flex items-center gap-2 bg-sky/50 backdrop-blur-sm text-white border border-sky hover:border-peach px-4 py-2 rounded-md text-xl font-nunito hover:bg-sky/30 transition font-bold"
+              className="max-w-xl inline-flex items-center gap-2 bg-sky/50 backdrop-blur-sm text-white border border-sky hover:border-peach px-4 py-2 rounded-md font-nunito hover:bg-sky/30 transition font-bold text-2xl"
             >
               Galerij <ArrowRightIcon className="w-5 h-5" />
             </Link>
-          </div>
-      
-          
+          </div>       
         </div>
       </div>
-     </div>
+ 
+
+    <div className="max-w-7xl mx-auto mt-3 bg-banana p-10 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-pangaia font-semibold text-peach mb-6">
+        Ik hoor graag van je! 
+      </h2>
+      <form className="space-y-6">
+        <input
+          type="text"
+          placeholder="Naam"
+          className="font-nunito w-full border border-gray-300 rounded px-4 py-3 focus:outline-none focus:ring-2 focus:ring-peach text-xl"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="font-nunito w-full border border-gray-300 rounded px-4 py-3 focus:outline-none focus:ring-2 focus:ring-peach text-xl"
+        />
+        <textarea
+          placeholder="Bericht"
+          rows="5"
+          className="font-nunito w-full border border-gray-300 rounded px-4 py-3 focus:outline-none focus:ring-2 focus:ring-peach text-xl"
+        />
+        <button
+          type="submit"
+          className="font-nunito bg-sky text-white rounded px-6 py-3 font-bold hover:bg-sky/80 transition text-xl"
+        >
+          Verstuur
+        </button>
+      </form>
+    </div>
+    
+    </div>
+ 
     </>
   )
 };

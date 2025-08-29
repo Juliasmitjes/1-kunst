ce0c001b03a3a3324250b38b1d4319acea2cddec
lilJ <julia.s@live.nl>
2025-08-29 10:14:11 +0200

    add images

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -56,6 +56,7 @@ const Gallery = () => {
             />
           </div>
         </div>
+
         <div  className="max-w-7xl mx-auto mt-3 bg-peach text-left p-4 pl-10 rounded-lg">
         <div className={styles.header}>
           <h1 id="textDiscover" className="pt-20 pb-3 tracking-tight text-balance text-6xl text-white">
@@ -77,77 +78,77 @@ const Gallery = () => {
 
        <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-3 items-stretch">
 
-      <div
-        className={`bg-banana p-4 pl-10 rounded-lg overflow-hidden transition-all duration-500 ${
-          open ? "max-h-[630px]" : "max-h-[300px]"
-        }`}
-            >
-              <div className={styles.text}>
-                <p className="mt-8 pb-10 pr-6">
-                  Van groene joekel tot verdord skelet
-                </p>
-                <p>
-                  Kerst het feest van licht, goud en glitters én van kerstbomen. Bij
-                  ons thuis altijd twee stuks...
-                  {open && (
-                    <>
-                      {" "}
-                      Nordmannen, een kleintje in de voorkamer en een grote joekel in
-                      de achterkamer. Bos in huis, dennengeur, het groen, zo fijn, maar
-                      ook het trieste vooruitzicht: over een paar weken de bomen
-                      afgedankt bij het vuil als verdorde skeletten.
-                    </>
-                  )}
-                </p>
-                <button
-                  onClick={() => setOpen(!open)}
-                  className="mt-4 text-green-700 hover:text-green-900 transition"
-                >
-                  {open ? "▲ Minder" : "▼ Meer"}
-                </button>
-              </div>
-            </div>
-
-            <div
-        className={`bg-banana p-4 pl-10 rounded-lg overflow-hidden transition-all duration-500 ${
-          open ? "max-h-[630px]" : "max-h-[300px]"
-        }`}
-            >
-              <div className={styles.text}>
-                <p className="mt-8 pb-10 pr-6">
-                   Zullen we eens zonder boom?
-                </p>
-                <p>
-                  ‘Zullen we eens zonder boom?’, probeerde ik. Protest bij man en kinderen...
-                  {open && (
-                    <>
-                      {" "}
-                     ‘Er zijn alternatieven, zoals een kunstboom.’ ‘Nee mam, hartstikke kitsch en zo’n nepboom is ook niet duurzaam’. Daarmee was ik het met ze eens. ‘Een constructie van steigerhout dan?’ ‘Lelijk, mam’. ‘Of een moderne minimalistische variant?’.  ‘Zo’n kaal ding, dan kunnen we beter helemaal niets doen.’ En ook daarin gaf ik ze gelijk. En toch liet het me niet los. 
-                    </>
-                  )}
-                </p>
-                <button
-                  onClick={() => setOpen(!open)}
-                  className="mt-4 text-green-700 hover:text-green-900 transition"
-                >
-                  {open ? "▲ Minder" : "▼ Meer"}
-                </button>
-              </div>
-            </div>
-   
-
-          <div className="bg-peach p-4 pl-10 rounded-lg max-h-[630px] overflow-hidden" >
+        <div className="bg-banana p-4 pl-10 rounded-lg max-h-[700px] overflow-hidden" >
             <div className={styles.text}>
-            <p className="mt-8 pb-10 pr-6 text-white">
-            Uitdaging</p>  
-            <p className='text-white'>
-             Ik dacht aan de glinsterende kiezels in mijn jeugd. Er is zoveel schoonheid.  Het moet mogelijk zijn om een beter alternatief te bedenken. Ik ga het proberen! Ik zei tegen man en kinderen, ‘ik  ga de uitdaging aan.  Ik bedenk een feestelijke lichtbron als alternatief voor een boom: eenvoudig, minimaal materiaalgebruik (duurzaam) én met een wow-effect. Het beeld tekende zich in mijn hoofd af, maar de weg ernaartoe was een tocht met hobbels over constructies ontdekkingen over fournituren, lijmen, papiersoorten en stoffen. Uiteindelijk is het gelukt! 
+            <p className="mt-14 pb-10 pr-6 text-peach font-pangaia text-2xl">
+            Van groene joekel tot verdord skelet</p>  
+            <p className='text-gray-500 mb-24'>
+             Kerst het feest van licht, goud en glitters én van kerstbomen. Bij ons thuis altijd twee stuks, Nordmannen, een kleintje in de voorkamer en een grote joekel in de achterkamer. Bos in huis, dennengeur, het groen,  zo fijn, maar ook het trieste vooruitzicht: over een paar weken de bomen afgedankt bij het vuil als verdorde skeletten. 
             </p>                     
             </div>              
           </div>
-        
+
+           <div className="bg-banana p-4 pl-10 rounded-lg max-h-[700px] overflow-hidden" >
+            <div className={styles.text}>
+            <p className="mt-14 pb-10 pr-6 text-peach font-pangaia text-2xl">
+            Zullen we eens zonder boom?</p>  
+            <p className='text-gray-500'>
+            ‘Zullen we eens zonder boom?’, probeerde ik. Protest bij man en kinderen.  ‘Er zijn alternatieven, zoals een kunstboom.’ ‘Nee mam, hartstikke kitsch en zo’n nepboom is ook niet duurzaam’. Daarmee was ik het met ze eens. ‘Een constructie van steigerhout dan?’ ‘Lelijk, mam’. ‘Of een moderne minimalistische variant?’.  ‘Zo’n kaal ding, dan kunnen we beter helemaal niets doen.’ En ook daarin gaf ik ze gelijk. En toch liet het me niet los. 
+            </p>                     
+            </div>              
+          </div>   
+
+          <div className="bg-peach p-4 pl-10 rounded-lg max-h-[700px] overflow-hidden" >
+            <div className={styles.text}>
+            <p className="mt-14 pb-10 pr-6 text-white font-pangaia text-2xl">
+            Uitdaging</p>  
+            <p className='text-white pb-24'>
+             Ik dacht aan de glinsterende kiezels in mijn jeugd. Er is zoveel schoonheid.  Het moet mogelijk zijn om een beter alternatief te bedenken. Ik ga het proberen! Ik zei tegen man en kinderen, ‘ik  ga de uitdaging aan.  Ik bedenk een feestelijke lichtbron als alternatief voor een boom: eenvoudig, minimaal materiaalgebruik (duurzaam) én met een wow-effect. Het beeld tekende zich in mijn hoofd af, maar de weg ernaartoe was een tocht met hobbels over constructies ontdekkingen over fournituren, lijmen, papiersoorten en stoffen... 
+            </p>                     
+            </div>              
+          </div>
         </div>
       </div>
+
+      <div className="max-w-7xl mx-auto grid items-stretch mt-3">      
+        <div className="bg-banana p-4 pl-10 rounded-lg overflow-hidden" >
+          <div className={styles.header}>
+            <h1 className="pt-24 text-balance pb-10 text-6xl text-peach">
+              Uiteindelijk is het gelukt
+            </h1> 
+          </div>
+
+      <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-3 items-stretch mt-3">
+        <div className="relative group max-h-[630px] overflow-hidden rounded-md">
+            <img
+              className="w-full h-full object-cover brightness-125 group-hover:scale-125 transition duration-300"
+              src={`${process.env.PUBLIC_URL}/images/boom1.jpg`}
+              alt="Bomen"
+            />
+        </div>
+
+        <div className="relative group max-h-[630px] overflow-hidden rounded-md"> 
+          <img
+            className="w-full h-full object-cover brightness-125 group-hover:scale-125 transition duration-300"
+            src={`${process.env.PUBLIC_URL}/images/boom2.jpg`}
+            alt="Bomen"
+          />
+        </div>
+      </div>  
+
+       <div className="max-w-7xl mx-auto gap-3 items-stretch mt-3">
+        <div className="relative group max-h-[1500px] overflow-hidden rounded-md">
+            <img
+              className="w-full h-full object-cover brightness-125 group-hover:scale-125 transition duration-300"
+              src={`${process.env.PUBLIC_URL}/images/boom1.jpg`}
+              alt="Bomen"
+            />
+        </div>
+      </div>
+
+            
+          </div>
+        </div>
       </div>
     </>
   )
